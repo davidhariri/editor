884d4f10d955a31f4b6c6af66b9fd1085388db36
David Hariri <david.hariri@gmail.com>
2016-03-25 15:43:48 -0400

    fixes #23

--- source/jsx/BrowserListItem.js
+++ source/jsx/BrowserListItem.js
@@ -18,9 +18,17 @@ const BrowserListItem = React.createClass({
         }
 
         const articleTitle = this.props.article.title ? this.props.article.title : 'Untitled';
+        let articleStatus = "";
+
+        if(this.props.article.shared) {
+            articleStatus = "S";
+        } else if(this.props.article.published) {
+            articleStatus = "P";
+        }
 
         const template = (
             <div className={`browser__list__item ${selected}`} key={this.props.id} onClick={this.passArticleUp}>
+                <div className="browser__list__item__status">{articleStatus}</div>
                 <div className="browser__list__item__title">{articleTitle}</div>
                 <div className="browser__list__item__subtitle">{this.renderDate()}</div>
             </div>
